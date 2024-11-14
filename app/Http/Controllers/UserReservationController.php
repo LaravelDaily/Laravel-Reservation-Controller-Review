02bcba1ab55baa3f6b70afaf9a27da3b2c663b98
PovilasKorop <povilask007@gmail.com>
2024-11-14 07:14:18 +0200

    More validation and price calculation in Service

--- app/Http/Controllers/UserReservationController.php
+++ app/Http/Controllers/UserReservationController.php
@@ -30,21 +30,21 @@ class UserReservationController extends Controller
     public function store(StoreReservationRequest $request, OfficeService $officeService): ReservationResource
     {
         $data = $request->validated();
-        $office = $officeService->getOfficeDataForReservation($data['office_id'], auth()->id());
+        $office = $officeService->getOfficeDataForReservation(
+            $data['office_id'],
+            auth()->id(),
+            $data['start_date'],
+            $data['end_date']
+        );
 
-        $reservation = Cache::lock('reservations_office_' . $office->id, 10)->block(3, function () use ($data, $office) {
-            $numberOfDays = round(Carbon::parse($data['end_date'])->endOfDay()->diffInDays(Carbon::parse($data['start_date'])->startOfDay()) + 1) * -1;
+        $price = $officeService->getReservationPrice(
+            $office->price_per_day,
+            $office->monthly_discount,
+            $data['start_date'],
+            $data['end_date']
+        );
 
-            if ($office->reservations()->activeBetween($data['start_date'], $data['end_date'])->exists()) {
-                throw ValidationException::withMessages(['office_id' => 'You cannot make a reservation during this time']);
-            }
-
-            $price = $numberOfDays * $office->price_per_day;
-
-            if ($numberOfDays >= 28 && $office->monthly_discount) {
-                $price = $price * ((100 - $office->monthly_discount) / 100);
-            }
-
+        $reservation = Cache::lock('reservations_office_' . $office->id, 10)->block(3, function () use ($data, $office, $price) {
             return Reservation::create([
                 'user_id' => auth()->id(),
                 'office_id' => $office->id,
--- app/Services/OfficeService.php
+++ app/Services/OfficeService.php
@@ -3,11 +3,12 @@
 namespace App\Services;
 
 use App\Models\Office;
+use Carbon\Carbon;
 use Illuminate\Validation\ValidationException;
 
 class OfficeService
 {
-    public function getOfficeDataForReservation(int $officeId, int $userId): Office|ValidationException
+    public function getOfficeDataForReservation(int $officeId, int $userId, string $startDate, string $endDate): Office|ValidationException
     {
         $office = Office::findOrFail($officeId);
 
@@ -19,6 +20,23 @@ class OfficeService
             throw ValidationException::withMessages(['office_id' => 'You cannot make a reservation on a hidden or unapproved office']);
         }
 
+        if ($office->reservations()->activeBetween($startDate, $endDate)->exists()) {
+            throw ValidationException::withMessages(['office_id' => 'You cannot make a reservation during this time']);
+        }
+
         return $office;
+    }
+
+    public function getReservationPrice(int|float $pricePerDay, int|float $monthlyDiscount, string $startDate, string $endDate): int|float
+    {
+        $numberOfDays = round(Carbon::parse($endDate)->endOfDay()->diffInDays(Carbon::parse($startDate)->startOfDay()) + 1) * -1;
+
+        $price = $numberOfDays * $pricePerDay;
+
+        if ($numberOfDays >= 28 && $monthlyDiscount) {
+            $price = $price * ((100 - $monthlyDiscount) / 100);
+        }
+
+        return $price;
     }
 }
